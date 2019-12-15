5ba40f96f7ef66f778417704a0ef49ccb309b900
魏晨 <juanchengweichen@163.com>
2019-12-15 11:18:49 +0800

    intellij第一次与码云互联

---
+++ src/HelloWord.java
@@ -0,0 +1,11 @@
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("firest Test");
+        System.out.println("args = " + args);
+        int j=23;
+        System.out.println("j = " + j);
+    }
+
+
+}
